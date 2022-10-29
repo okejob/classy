bf709ec77921abcc02957ff5c835fff94a29e96d
Arjunanda <vincentiusmandala@gmail.com>
2022-10-30 00:54:48 +0700

    Modified By at Item Note

--- app/Models/Transaksi/ItemNote.php
+++ app/Models/Transaksi/ItemNote.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Transaksi;
 
+use App\Models\User;
 use App\Observers\UserActionObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,10 +11,24 @@ class ItemNote extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+    protected $appends = [
+        'nama_user'
+    ];
+
+    public function getNamaUserAttribute()
+    {
+        $user = User::find($this->modified_by);
+        return $user->username;
+    }
 
     public static function boot()
     {
         parent::boot();
         ItemNote::observe(new UserActionObserver);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
