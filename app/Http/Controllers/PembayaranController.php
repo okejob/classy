85305a75c433f1735fe0276e1b6d855a0321bc77
Arjunanda <vincentiusmandala@gmail.com>
2022-12-15 12:55:03 +0700

    Status Transaksi

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pembayaran;
+use App\Models\Transaksi\Transaksi;
 use Illuminate\Http\Request;
 
 class PembayaranController extends Controller
@@ -12,12 +13,23 @@ class PembayaranController extends Controller
         $request->validate([
             'nominal' => 'required|integer'
         ]);
+
         Pembayaran::create([
             'nominal' => $request->nominal,
             'transaksi_id' => $request->transaksi_id,
             'saldo_id' => $request->saldo_id,
             'metode_pembayaran' => $request->metode_pembayaran
         ]);
+
+        $transaksi = Transaksi::find($request->transaksi_id);
+        $total_terbayar = $transaksi->total_terbayar + (int) $request->nominal;
+        if ($total_terbayar >= $transaksi->grand_total) {
+            $transaksi->total_terbayar = $transaksi->grand_total;
+            $transaksi->lunas = true;
+        } else {
+            $transaksi->total_terbayar = $total_terbayar;
+        }
+        $transaksi->save();
 
         return redirect()->intended(route('menu_pembayaran'));
     }
@@ -35,6 +47,7 @@ class PembayaranController extends Controller
         $request->validate([
             'nominal' => 'required|integer'
         ]);
+
         $pembayaran->update([
             'nominal' => $request->nominal,
             'transaksi_id' => $request->transaksi_id,
