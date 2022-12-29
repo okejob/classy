1bec61ebdd8c65d8d12520d6d75bb815e487c62e
Arjunanda <vincentiusmandala@gmail.com>
2022-12-29 12:05:07 +0700

    Inventory Part 1

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Inventory\Inventory;
+use Illuminate\Http\Request;
+
+class InventoryController extends Controller
+{
+    public function insert(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|string',
+            'stok' => 'numeric|min:0'
+        ]);
+
+        Inventory::create([
+            'nama' => $request->nama,
+            'deskripsi' => $request->deskripsi,
+            'stok' => $request->stok
+        ]);
+
+        return redirect()->intended(route('menu-inventory'));
+    }
+
+    public function update(Request $request, Inventory $inventory)
+    {
+        $request->validate([
+            'nama' => 'required|string',
+            'stok' => 'numeric|min:0'
+        ]);
+
+        $inventory->update([
+            'nama' => $request->nama,
+            'deskripsi' => $request->deskripsi,
+            'stok' => $request->stok
+        ]);
+
+        return redirect()->intended(route('menu-inventory'));
+    }
+
+    public function delete(Inventory $inventory)
+    {
+        $inventory->delete();
+
+        return [
+            'status' => 200,
+            'message' => 'Success'
+        ];
+    }
+}
---
+++ database/migrations/2022_12_29_041027_create_laporan_inventories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laporan_inventories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('inventory_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->integer('jumlah');
+            $table->foreignId('modified_by')
+                ->constrained('users', 'id')
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laporan_inventories');
+    }
+};
