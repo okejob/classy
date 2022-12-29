059487ff039589858ca3cd7b14ef0da63cceb3c9
Mitchell Arthur <mitchell2@mhs.stts.edu>
2022-12-29 12:16:27 +0700

    Update PickupDelivery.blade.php

--- resources/views/pages/transaksi/PickupDelivery.blade.php
+++ resources/views/pages/transaksi/PickupDelivery.blade.php
@@ -7,12 +7,17 @@
         <i class="fas fa-angle-right mx-2"></i>
         <a>Pickup &amp; Delivery</a>
     </header>
-    <ul role="tablist" class="nav nav-tabs border-bottom-0">
-        <li role="presentation" class="nav-item"><a role="tab" data-bs-toggle="tab" class="nav-link" href="#tab-1">Data</a></li>
-        <li role="presentation" class="nav-item"><a role="tab" data-bs-toggle="tab" class="nav-link active" href="#tab-2">Task Hub</a></li>
+
+    <ul role="tablist" class="nav nav-tabs position-relative border-bottom-0">
+        <li role="presentation" class="nav-item"><a role="tab" data-bs-toggle="tab" class="nav-link active" href="#tab-1">Data</a></li>
+        <li role="presentation" class="nav-item"><a role="tab" data-bs-toggle="tab" class="nav-link" href="#tab-2">Task Hub</a></li>
+        <div class="d-flex align-items-center position-absolute end-0" style="bottom: -1px;">
+            <h6 class="me-2">Tanggal</h6>
+            <input type="date" name="input-date" class="form-control" style="width: 200px;">
+        </div>
     </ul>
     <div class="tab-content">
-        <div role="tabpanel" class="tab-pane" id="tab-1">
+        <div role="tabpanel" class="tab-pane active" id="tab-1">
             <div class="card">
                 <div class="card-body">
                     <section id="section-pickup" class="mb-4">
@@ -41,87 +46,86 @@
                 </div>
             </div>
         </div>
-        <div role="tabpanel" class="tab-pane active" id="tab-2">
-            <div class="p-3 border rounded mb-3" style="margin: 0 .375rem 1rem!important;">
-                <input type="date" name="input-date" class="form-control">
-            </div>
-            @foreach ($dataDriver as $driver)
-            <div class="card-1 w-50 float-start" style="padding: 0 .375rem!important;">
-                <div class="p-3 border rounded">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <h4>Hub {{ $driver->username }}</h4>
-                        <button class="btn btn-sm btn-toggle" style="box-shadow: none;"><i class="fa-solid fa-down-left-and-up-right-to-center"></i></button>
-                    </div>
-                    <div class="hub-container">
-                        <hr />
-                        <div class="hub-list">
-                            @foreach ($pickups as $pickup)
-                                @if($driver->id == $pickup->driver_id)
-                                    <div class="p-3 border rounded d-flex justify-content-between align-items-center mt-3"
-                                        @if ($pickup->is_done)
-                                            style="border-bottom: 3px solid rgb(75, 192, 192)!important; background-image: linear-gradient(to bottom right, white, rgb(75, 192, 192, .5));"
-                                        @else
-                                            style="border-bottom: 3px solid rgb(75, 192, 192)!important;"
-                                        @endif
-                                    >
-                                        <div id="{{ $pickup->id }}" class="d-flex flex-column">
-                                            <h4>{{ $pickup->kode }}</h4>
-                                            <h6>
-                                                <span class="text-muted">{{ $pickup->pelanggan->nama }}</span>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 16 16" fill="currentColor" class="bi bi-dot">
-                                                    <path fill-rule="evenodd" d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path>
-                                                </svg>
-                                                {{ $pickup->alamat }}
-                                            </h6>
-                                        </div>
-                                        <div class="position-relative">
-                                            <h4 class="fw-bold" style="font-style: italic;">Pickup</h4>
+        <div role="tabpanel" class="tab-pane" id="tab-2">
+            <div class="row">
+                @foreach ($dataDriver as $driver)
+                <div class="col-6">
+                    <div class="border rounded p-3">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h4>Hub {{ $driver->username }}</h4>
+                            <button class="btn btn-sm btn-toggle" style="box-shadow: none;"><i class="fa-solid fa-down-left-and-up-right-to-center"></i></button>
+                        </div>
+                        <div class="hub-container">
+                            <hr />
+                            <div class="hub-list">
+                                @foreach ($pickups as $pickup)
+                                    @if($driver->id == $pickup->driver_id)
+                                        <div class="p-3 border rounded d-flex justify-content-between align-items-center mt-3"
                                             @if ($pickup->is_done)
-                                                <i class="fa-solid fa-flag-checkered position-absolute top-50 start-0 translate-middle fa-4x" style="font-style: italic; opacity: 0.25;"></i>
+                                                style="border-bottom: 3px solid rgb(75, 192, 192)!important; background-image: linear-gradient(to bottom right, white, rgb(75, 192, 192, .5));"
                                             @else
-                                                <i class="fa-solid fa-spinner position-absolute top-50 start-0 translate-middle fa-4x" style="font-style: italic; opacity: 0.25;"></i>
+                                                style="border-bottom: 3px solid rgb(75, 192, 192)!important;"
                                             @endif
+                                        >
+                                            <div id="{{ $pickup->id }}" class="d-flex flex-column">
+                                                <h4>{{ $pickup->kode }}</h4>
+                                                <h6>
+                                                    <span class="text-muted">{{ $pickup->pelanggan->nama }}</span>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 16 16" fill="currentColor" class="bi bi-dot">
+                                                        <path fill-rule="evenodd" d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path>
+                                                    </svg>
+                                                    {{ $pickup->alamat }}
+                                                </h6>
+                                            </div>
+                                            <div class="position-relative">
+                                                <h4 class="fw-bold" style="font-style: italic;">Pickup</h4>
+                                                @if ($pickup->is_done)
+                                                    <i class="fa-solid fa-flag-checkered position-absolute top-50 start-0 translate-middle fa-4x" style="font-style: italic; opacity: 0.25;"></i>
+                                                @else
+                                                    <i class="fa-solid fa-spinner position-absolute top-50 start-0 translate-middle fa-4x" style="font-style: italic; opacity: 0.25;"></i>
+                                                @endif
+                                            </div>
                                         </div>
-                                    </div>
-                                @endif
-                            @endforeach
+                                    @endif
+                                @endforeach
 
-                            @foreach ($deliveries as $delivery)
-                                @if($driver->id == $delivery->driver_id)
-                                    <div class="p-3 border rounded d-flex justify-content-between align-items-center mt-3"
-                                        @if ($delivery->is_done)
-                                            style="border-bottom: 3px solid rgb(153, 102, 255)!important; background-image: linear-gradient(to bottom right, white, rgb(153, 102, 255, .5));"
-                                        @else
-                                            style="border-bottom: 3px solid rgb(153, 102, 255)!important;"
-                                        @endif
-                                    >
-                                        <div id="{{ $delivery->id }}" class="d-flex flex-column">
-                                            <h4>{{ $delivery->kode }}</h4>
-                                            <h6>
-                                                <span class="text-muted">{{ $delivery->pelanggan->nama }}</span>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 16 16" fill="currentColor" class="bi bi-dot">
-                                                    <path fill-rule="evenodd" d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path>
-                                                </svg>
-                                                {{ $delivery->alamat }}
-                                            </h6>
-                                        </div>
-                                        <div class="position-relative">
-                                            {{-- background-image: linear-gradient(to bottom right, red, yellow); --}}
-                                            <h4 class="fw-bold" style="font-style: italic;">Delivery</h4>
+                                @foreach ($deliveries as $delivery)
+                                    @if($driver->id == $delivery->driver_id)
+                                        <div class="p-3 border rounded d-flex justify-content-between align-items-center mt-3"
                                             @if ($delivery->is_done)
-                                                <i class="fa-solid fa-flag-checkered position-absolute top-50 start-0 translate-middle fa-4x" style="font-style: italic; opacity: 0.25;"></i>
+                                                style="border-bottom: 3px solid rgb(153, 102, 255)!important; background-image: linear-gradient(to bottom right, white, rgb(153, 102, 255, .5));"
                                             @else
-                                                <i class="fa-solid fa-spinner position-absolute top-50 start-0 translate-middle fa-4x" style="font-style: italic; opacity: 0.25;"></i>
+                                                style="border-bottom: 3px solid rgb(153, 102, 255)!important;"
                                             @endif
+                                        >
+                                            <div id="{{ $delivery->id }}" class="d-flex flex-column">
+                                                <h4>{{ $delivery->kode }}</h4>
+                                                <h6>
+                                                    <span class="text-muted">{{ $delivery->pelanggan->nama }}</span>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 16 16" fill="currentColor" class="bi bi-dot">
+                                                        <path fill-rule="evenodd" d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path>
+                                                    </svg>
+                                                    {{ $delivery->alamat }}
+                                                </h6>
+                                            </div>
+                                            <div class="position-relative">
+                                                {{-- background-image: linear-gradient(to bottom right, red, yellow); --}}
+                                                <h4 class="fw-bold" style="font-style: italic;">Delivery</h4>
+                                                @if ($delivery->is_done)
+                                                    <i class="fa-solid fa-flag-checkered position-absolute top-50 start-0 translate-middle fa-4x" style="font-style: italic; opacity: 0.25;"></i>
+                                                @else
+                                                    <i class="fa-solid fa-spinner position-absolute top-50 start-0 translate-middle fa-4x" style="font-style: italic; opacity: 0.25;"></i>
+                                                @endif
+                                            </div>
                                         </div>
-                                    </div>
-                                @endif
-                            @endforeach
+                                    @endif
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
     </div>
 
