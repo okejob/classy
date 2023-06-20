7a435e6cc1a82429719803e25f825793eda0a351
Mitchell Arthur <mitchell2@mhs.stts.edu>
2023-06-20 15:51:52 +0700

    Update head.blade.php

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 <meta name="csrf-token" content="{{ csrf_token() }}" />
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 <title>Classy</title>
 <?php header('Access-Control-Allow-Origin: *'); ?>
