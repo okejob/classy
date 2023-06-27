6e66e53e5713ce8be0f755192ff52450edf211fc
Mitchell Arthur <mitchell2@mhs.stts.edu>
2023-06-27 15:02:21 +0700

    Update UploadTrait.php

--- app/Http/Traits/UploadTrait.php
+++ app/Http/Traits/UploadTrait.php
@@ -11,18 +11,18 @@ trait UploadTrait
     //Function Global untuk mengupload Gambar
     public function upload(Request $request, $path)
     {
-        $quality = 70;
+        // $quality = 70;
         $default_path = 'image/' . $path . '/';
         $file = $request->file('image');
         if ($file) {
-            $file_name = time() . '.' . $file->getClientOriginalExtension();
-            $final_path = $default_path . $file_name;
+            // $file_name = time() . '.' . $file->getClientOriginalExtension();
+            // $final_path = $default_path . $file_name;
             // $img = Image::make($file);
             // $img->save($final_path, $quality);
 
             // return $final_path;
-            Storage::disk('digitalocean')->put('/Penerima/' . $file_name, $file, 'public');
-            $url = Storage::disk('digitalocean')->url('/Penerima' . $file_name);
+            $file_url = Storage::disk('digitalocean')->put($default_path, $file, $file);
+            $url = Storage::disk('digitalocean')->url($file_url);
             return $url;
         }
         return null;
